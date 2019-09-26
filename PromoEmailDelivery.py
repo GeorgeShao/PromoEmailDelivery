fdaa2a4964efbd9d28d412fc4b86500102a6a0f2
George Shao <GeorgeShao123@gmail.com>
2019-09-26 19:18:20 -0400

    Finished SMTP server code. Email sending fully works!

--- PromoEmailDelivery.py
+++ PromoEmailDelivery.py
@@ -4,8 +4,8 @@ import json
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 
-USERNAME = ""
-PASSWORD = ""
+SENDER_EMAIL = ""
+SENDER_PASSWORD = ""
 
 # Reads contacts from contacts.txt & returns names & emails
 def get_contacts():
@@ -26,31 +26,31 @@ def read_template():
 
 
 # Setup SMTP (Simple Mail Transfer Protocol) server
-def setup_server():
-    with open("CREDIDENTIALS.json", mode='r', encoding='utf-8') as credidentials_file:
-        data = json.load(credidentials_file)
-        USERNAME = data["USERNAME"]
-        PASSWORD = data["PASSWORD"]
-        print(f"USERNAME: {USERNAME}")
-        print(f"PASSWORD: {PASSWORD}")
+with open("CREDIDENTIALS.json", mode='r', encoding='utf-8') as credidentials_file:
+    data = json.load(credidentials_file)
+    SENDER_EMAIL = data["SENDER_EMAIL"]
+    SENDER_PASSWORD = data["SENDER_PASSWORD"]
+    print(f"SENDER_EMAIL: {SENDER_PASSWORD}")
+    print(f"SENDER_PASSWORD: {SENDER_PASSWORD}")
 
-    s = smtplib.SMTP(host='smtp.gmail.com', port=587)
-    s.starttls()
-    s.login(USERNAME, PASSWORD)
+s = smtplib.SMTP(host='smtp.gmail.com', port=587)
+s.starttls()
+s.login(SENDER_EMAIL, SENDER_PASSWORD)
 
 
-setup_server()  # setup server
 contact_names, contact_emails = get_contacts()  # get contacts
 message_template = read_template()  # read template
+
 
 # For each contact, send the custom email
 for contact_name, contact_email in zip(contact_names, contact_emails):
     msg = MIMEMultipart()  # create a message
-    message = message_template.substitute(CONTACT_NAME=name.title())
+    message = message_template.substitute(CONTACT_NAME=contact_name.title())
     msg['From']=SENDER_EMAIL
     msg['To']=contact_email
     msg['Subject']="Subject Text Goes Here"
     msg.attach(MIMEText(message, 'plain'))
     s.send_message(msg)
+    print(f"Msg sent to {contact_name}: {contact_email}")
     
     del msg
