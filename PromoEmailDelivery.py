49483945d68ed393a6c2dd23d0cf52323d40ba0c
George Shao <GeorgeShao123@gmail.com>
2019-09-26 19:49:34 -0400

    Got contact info from csv file.

--- PromoEmailDelivery.py
+++ PromoEmailDelivery.py
@@ -1,3 +1,4 @@
+import csv
 from string import Template
 import smtplib
 import json
@@ -11,13 +12,25 @@ SENDER_PASSWORD = ""
 def get_contacts():
     names = []
     emails = []
-    with open("contacts.txt", mode='r', encoding='utf-8') as contacts_file:
-        for contact in contacts_file:
-            names.append(contact.split()[0])
-            emails.append(contact.split()[1])
-    return names, emails
+    with open("test_data.csv", mode='r', encoding='utf-8') as csv_file:
+        csv_file_content = csv.reader(csv_file, delimiter=',')
+        first_line = True
+        for contact in csv_file_content:
+            if not first_line:
+                if contact[0]:
+                    print(f"{contact[0]}: {contact[1]} {contact[2]}")
+            else:
+                first_line = False
 
+    # with open("contacts.txt", mode='r', encoding='utf-8') as contacts_file:
+    #     for contact in contacts_file:
+    #         names.append(contact.split()[0])
+    #         emails.append(contact.split()[1])
+    # return names, emails
 
+get_contacts()
+
+'''
 # Reads template from message.txt & returns template object
 def read_template():
     with open("message.txt", mode='r', encoding='utf-8') as template_file:
@@ -30,17 +43,20 @@ with open("CREDIDENTIALS.json", mode='r', encoding='utf-8') as credidentials_fil
     data = json.load(credidentials_file)
     SENDER_EMAIL = data["SENDER_EMAIL"]
     SENDER_PASSWORD = data["SENDER_PASSWORD"]
-    print(f"SENDER_EMAIL: {SENDER_PASSWORD}")
-    print(f"SENDER_PASSWORD: {SENDER_PASSWORD}")
+    print("Obtained credidentials")
 
 s = smtplib.SMTP(host='smtp.gmail.com', port=587)
 s.starttls()
 s.login(SENDER_EMAIL, SENDER_PASSWORD)
+print("Logged into SMTP server")
 
 
 contact_names, contact_emails = get_contacts()  # get contacts
-message_template = read_template()  # read template
+print("Received contact list")
 
+
+message_template = read_template()  # read template
+print("Received message template")
 
 # For each contact, send the custom email
 for contact_name, contact_email in zip(contact_names, contact_emails):
@@ -54,3 +70,4 @@ for contact_name, contact_email in zip(contact_names, contact_emails):
     print(f"Msg sent to {contact_name}: {contact_email}")
     
     del msg
+'''
